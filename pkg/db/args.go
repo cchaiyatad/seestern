47ffa7e387092bc918f538ba069aa1576ba0590b
cchaiyatad <chaiyatad@gmail.com>
2021-12-08 11:26:59 +0700

    feat: add ParseCollectionInputFromArgs

---
+++ pkg/db/args.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ParseCollectionInputFromArgs(args []string) map[string][]string {
+	output := make(map[string][]string)
+
+	for _, arg := range args {
+		spilted_arg := strings.SplitN(arg, ".", 2)
+		if len(spilted_arg) != 2 {
+			continue
+		}
+
+		db := spilted_arg[0]
+		coll := spilted_arg[1]
+
+		if !contains(output[db], coll) {
+			output[db] = append(output[db], coll)
+		}
+	}
+	fmt.Println(output)
+	return output
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
---
+++ pkg/db/args_test.go
@@ -0,0 +1,76 @@
+package db
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseCollectionInputFromArgs(t *testing.T) {
+	t.Parallel()
+
+	t.Run("ParseCollectionInputFromArgs on empty slice", func(t *testing.T) {
+		givenArgs := []string{}
+
+		expected := map[string][]string{}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+	t.Run("ParseCollectionInputFromArgs on one correct item", func(t *testing.T) {
+		givenArgs := []string{"db1.coll1"}
+
+		expected := map[string][]string{"db1": {"coll1"}}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+	t.Run("ParseCollectionInputFromArgs on two correct item with same db", func(t *testing.T) {
+		givenArgs := []string{"db1.coll1", "db1.coll2"}
+
+		expected := map[string][]string{"db1": {"coll1", "coll2"}}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+	t.Run("ParseCollectionInputFromArgs on two correct item with same db same coll", func(t *testing.T) {
+		givenArgs := []string{"db1.coll1", "db1.coll1"}
+
+		expected := map[string][]string{"db1": {"coll1"}}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+	t.Run("ParseCollectionInputFromArgs on two correct item with same coll", func(t *testing.T) {
+		givenArgs := []string{"db1.coll1", "db2.coll1"}
+
+		expected := map[string][]string{"db1": {"coll1"}, "db2": {"coll1"}}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+	t.Run("ParseCollectionInputFromArgs on incorrect format", func(t *testing.T) {
+		givenArgs := []string{"db1coll1"}
+
+		expected := map[string][]string{}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+	t.Run("ParseCollectionInputFromArgs on two dot", func(t *testing.T) {
+		givenArgs := []string{"db1.coll1.sub"}
+
+		expected := map[string][]string{"db1": {"coll1.sub"}}
+		got := ParseCollectionInputFromArgs(givenArgs)
+
+		assert.Equal(t, true, reflect.DeepEqual(expected, got), fmt.Sprintf("expected: %s got: %s", expected, got))
+	})
+
+}
