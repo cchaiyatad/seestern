f55153ea3dd379b1f141cea4977e282037e041c1
cchaiyatad <chaiyatad@gmail.com>
2021-12-08 19:11:23 +0700

    feat: add config model

---
+++ pkg/cf/model.go
@@ -0,0 +1,52 @@
+package cf
+
+type Alias struct {
+	Key   string `json:"key" toml:"key"`
+	Value string `json:"value" toml:"value"`
+}
+
+type Database struct {
+	D_name      string     `json:"d_name" toml:"d_name"`
+	Collections Collection `json:"collections" toml:"collections"`
+}
+
+type Collection struct {
+	C_name string  `json:"c_name" toml:"c_name"`
+	Count  int     `json:"count" toml:"count"`
+	Fileds []Field `json:"fileds" toml:"fileds"`
+}
+
+type Field struct {
+	F_name      string       `json:"f_name" toml:"f_name"`
+	Omit_weight float64      `json:"omit_weight" toml:"omit_weight"`
+	Constraints []Constraint `json:"constraints" toml:"constraints"`
+	Sets        []Set        `json:"sets" toml:"sets"`
+}
+
+type Constraint struct {
+	Weight int `json:"weight" toml:"weight"`
+}
+
+type Set struct {
+	At    []int  `json:"at" toml:"at"`
+	Items []Item `json:"items" toml:"items"`
+}
+
+type Item struct {
+	Value Value `json:"value" toml:"value"`
+	Enum  Enum  `json:"enum" toml:"enum"`
+	Type  Type  `json:"type" toml:"type"`
+}
+
+type Value struct {
+	Value string `json:"value" toml:"value"`
+}
+
+type Enum struct {
+	Enum string `json:"enum" toml:"enum"`
+}
+
+type Type struct {
+	Type   string                 `json:"type" toml:"type"`
+	Params map[string]interface{} `json:"params" toml:"params"`
+}
