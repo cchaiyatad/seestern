52a3fb2b20c783b34c0b3b05e66684e01e63c4b7
cchaiyatad <chaiyatad@gmail.com>
2021-12-15 16:00:09 +0700

    test: configFileGenerator for object

--- pkg/cf/configFileGenerator_test.go
+++ pkg/cf/configFileGenerator_test.go
@@ -121,3 +121,56 @@ func TestConfigFileGeneratorBeginArray(t *testing.T) {
 		})
 	}
 }
+
+func TestConfigFileGeneratorBeginObject(t *testing.T) {
+	doc1 := &map[string]interface{}{"name": map[string]interface{}{"fname": "john"}}
+	doc2 := &map[string]interface{}{"name": map[string]interface{}{"fname": "johnson"}}
+	doc3 := &map[string]interface{}{"name": map[string]interface{}{"lname": "tomson"}}
+	doc4 := &map[string]interface{}{"name": map[string]interface{}{"fname": "tom", "age": 25}}
+
+	cases := []struct {
+		givenFileType string
+		givenDocs     []*map[string]interface{}
+		expected      *SSConfig
+	}{
+		{"yaml", []*map[string]interface{}{doc1}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc2}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc3}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "lname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc4}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "age", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Integer}}}}}, Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc1, doc2}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc1, doc3}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}, {Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "lname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc1, doc4}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}, {Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "age", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Integer}}}}}, Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc2, doc3}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}, {Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "lname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc2, doc4}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}, {Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "age", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Integer}}}}}, Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+		{"yaml", []*map[string]interface{}{doc3, doc4}, &SSConfig{Databases: []Database{{D_name: "DB", Collection: Collection{C_name: "Coll", Fields: []Field{{F_name: "name", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "lname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}, {Item: Item{Type: Type{Type: SS_Object, ElementType: []interface{}{Field{F_name: "age", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_Integer}}}}}, Field{F_name: "fname", Constraints: []Constraint{{Item: Item{Type: Type{Type: SS_String}}}}}}}}}}}}}}}}},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(fmt.Sprintf("ConfigFileGenerator with %v items", tc.givenDocs), func(t *testing.T) {
+			t.Parallel()
+			givenDBName := "DB"
+			givenCollName := "Coll"
+			givenGenerator := NewConfigFileGenerator(tc.givenFileType)
+
+			givenCallback, givenOnFinish := givenGenerator.Begin(givenDBName, givenCollName)
+
+			for _, doc := range tc.givenDocs {
+				givenCallback(*doc)
+			}
+
+			givenOnFinish()
+
+			go func() {
+				for range givenGenerator.OutChan {
+					givenGenerator.Done()
+				}
+			}()
+
+			givenGenerator.Wait()
+			givenGenerator.Close()
+
+			assert.Equal(t, tc.expected, givenGenerator.SSConfig)
+		})
+	}
+}
