b085e1a0ea3d6047cf41d3fae8ee4b7dfcb0ab38
cchaiyatad <chaiyatad@gmail.com>
2021-12-16 21:30:50 +0700

    test: NewDecoder

--- internal/dataformat/decoder.go
+++ internal/dataformat/decoder.go
@@ -47,9 +47,9 @@ func NewDecoder(filePath string) (*Decoder, error) {
 	case "json":
 		decoder.dec = &jsonUnmarshaler{}
 	case "toml":
-		decoder.dec = &yamlUnmarshaler{}
-	case "yaml":
 		decoder.dec = &tomlUnmarshaler{}
+	case "yaml":
+		decoder.dec = &yamlUnmarshaler{}
 	default:
 		return nil, ErrFileTypeNotSupport
 	}
@@ -80,20 +80,3 @@ func (d *tomlUnmarshaler) unmarshal(data []byte, v interface{}) error {
 	_, err := toml.Decode(string(data), v)
 	return err
 }
-
-// t.Run("test GetBytesFromFile with configuration file", func(t *testing.T) {
-// 	givenPath := "./../../test/config/04_configSpec_alias.ss.toml"
-
-// 	gotByte, _ := GetBytesFromFile(givenPath)
-// 	var ssConfig cf.SSConfig
-
-// 	// f, _ := os.Open(givenPath)
-// 	// dec := toml.NewDecoder(f)
-
-// 	// _, _ = dec.Decode(&ssConfig)
-// 	_, err := toml.Decode(string(gotByte), &ssConfig)
-
-// 	fmt.Printf("%s\n", err)
-// 	fmt.Printf("%#v\n", ssConfig)
-
-// })
--- internal/dataformat/decoder_test.go
+++ internal/dataformat/decoder_test.go
@@ -1,1 +1,66 @@
 package dataformat
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDecoder(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NewDecoder with valid file", func(t *testing.T) {
+		cases := []struct {
+			filePath            string
+			expectedDecoderType string
+			expectedData        string
+		}{
+			{"./test/decoder/file.json", "*dataformat.jsonUnmarshaler", "{\n    \"Name\": \"Platypus\",\n    \"Order\": \"Monotremata\"\n}"},
+			{"./test/decoder/file.yaml", "*dataformat.yamlUnmarshaler", "a: Easy!\nb:\n  c: 2\n  d: [3, 4]"},
+			{"./test/decoder/file.toml", "*dataformat.tomlUnmarshaler", "Age = 25\nCats = [ \"Cauchy\", \"Plato\" ]\nPi = 3.14\nPerfection = [ 6, 28, 496, 8128 ]"},
+			{"./test/decoder/file.ss.json", "*dataformat.jsonUnmarshaler", "{\n    \"Name\": \"Platypus\",\n    \"Order\": \"Monotremata\"\n}"},
+			{"./test/decoder/file.ss.yaml", "*dataformat.yamlUnmarshaler", "a: Easy!\nb:\n  c: 2\n  d: [3, 4]"},
+			{"./test/decoder/file.ss.toml", "*dataformat.tomlUnmarshaler", "Age = 25\nCats = [ \"Cauchy\", \"Plato\" ]\nPi = 3.14\nPerfection = [ 6, 28, 496, 8128 ]"},
+		}
+
+		for _, tc := range cases {
+			tc := tc
+			t.Run(fmt.Sprintf("NewDecoder with file type %s expected to get dec with type %s", tc.filePath, tc.expectedDecoderType), func(t *testing.T) {
+				t.Parallel()
+				gotDecoder, gotErr := NewDecoder(tc.filePath)
+
+				assert.Nil(t, gotErr)
+
+				gotDecType := reflect.TypeOf(gotDecoder.dec)
+				assert.Equal(t, tc.expectedDecoderType, gotDecType.String())
+				assert.Equal(t, tc.expectedData, string(gotDecoder.data))
+			})
+		}
+	})
+
+	t.Run("NewDecoder with invalid file", func(t *testing.T) {
+		cases := []struct {
+			filePath    string
+			expectedErr string
+		}{
+			{"./test/decoder/dir", "a given path is not a file: ./test/decoder/dir"},
+			{"./test/decoder/file", "error: not support file type (support only .json .toml .yaml)"},
+			{"./test/decoder/filejson", "error: not support file type (support only .json .toml .yaml)"},
+			{"./test/decoder/file-not-exist", "stat ./test/decoder/file-not-exist: no such file or directory"},
+		}
+
+		for _, tc := range cases {
+			tc := tc
+			t.Run(fmt.Sprintf("NewDecoder with file type %s expected to get error", tc.filePath), func(t *testing.T) {
+				t.Parallel()
+				gotDecoder, gotErr := NewDecoder(tc.filePath)
+
+				assert.Nil(t, gotDecoder)
+				assert.Equal(t, tc.expectedErr, gotErr.Error())
+			})
+		}
+	})
+
+}
